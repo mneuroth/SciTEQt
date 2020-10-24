edcc2d75ff86829f58baf40c69f32a0e14dc09a4
Michael Neuroth <michael.neuroth@freenet.de>
2020-10-24 11:05:06 +0200

    Updated version to 0.99.2

--- scite/qt/sciteqt.cpp
+++ scite/qt/sciteqt.cpp
@@ -45,7 +45,7 @@
 
 //*************************************************************************
 
-#define __SCITE_QT_VERSION__   "0.99.1"
+#define __SCITE_QT_VERSION__   "0.99.2"
 
 enum {
     WORK_EXECUTE = WORK_PLATFORM + 1,
